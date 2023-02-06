3b5b00b7ff80601705f629f136ccd8af6a906dc5
diogowolff <54316784+diogowolff@users.noreply.github.com>
2023-02-06 17:34:04 -0300

    not much

--- PS1/Matlab/Q1/Q1_itemB.m
+++ PS1/Matlab/Q1/Q1_itemB.m
@@ -1,2 +1,7 @@
 %% Data processing as done in Mincer (1962)
 
+dataset = dataset(dataset.RACE == 1 & dataset.MARST == 1, :);
+
+heads_working_index = dataset(dataset.RELATE == 1 & dataset.INCWAGE > 0, 'CBSERIAL');
+
+dataset = dataset(ismember(dataset(:,'CBSERIAL'), heads_working_index), :);
